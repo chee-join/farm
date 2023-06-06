a8918202248af4d65e11bf7bc15b673640987d80
chee <chiaki.shigure@gmail.com>
2023-06-06 22:52:43 +0900

    swiper修正

--- asset/script/main.js
+++ asset/script/main.js
@@ -5,7 +5,7 @@ $(function () {
         $("#slide-in").toggleClass('panelactive');
     });
 
-    $(".hamburger__menu-list a").click(function(){
+    $(".hamburger__menu-list a").click(function () {
         $(".hamburger-circle").removeClass('active');
         $("#slide-in").removeClass('panelactive');
     });
@@ -17,19 +17,20 @@ const swiper = new Swiper(".swiper", {
     loop: true,
     centeredSlides: true,
     slidesPerView: 1.5,
-    spaceBetween: 40,
+    spaceBetween: 24,
     autoplay: {
-        delay: 5000 //5秒ごとにスライダを切り替える
+        delay: 3000,
+        disableOnInteraction: false,
     },
-    speed: 1000,
-
+    speed: 500,
+    grabCursor: true,
     breakpoints: {
         768: {
-            slidesPerView: 3.8,
-            loop: true,
+            slidesPerView: 'auto',
+            spaceBetween: 24,
+            centeredSlides: false,
         },
     },
-
 });
 
 /*FAQ アコーディオン*/
@@ -39,4 +40,41 @@ $(function () {
         $(this).find('.open-icon').toggleClass('active');
     });
 });
+
+/*問い合わせフォーム*/
+$(document).ready(function () {
+    const $submitBtn = $('#submit__btn');
+
+    $('#form input,#form textarea').on('change', function () {
+        console.log('change');
+
+    });
+
+});
+
+// if (
+//     $('#form input[type="radio"]').val() !== "" &&
+//     $('#form input[type="text"]').val() !== "" &&
+//     $('#form input[type="email"]').val() === true
+
+// ) {
+//     $submitBtn.prop('disabled', false);
+
+// } else {
+//     $submitBtn.prop('disabled', true);
+// }
+
+/*スムーススクロール*/
+$(function () {
+    $('a[href^="#"]').click(function () {
+        const speed = 600;
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? "html" : href);
+        let position = target.offset().top;
+        $("body,html").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+});
+
+/*ローディング*/
 
